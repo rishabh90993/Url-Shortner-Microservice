5211dbd9df251a0104f3dbd4ff9c7653a9f7d595
Rishabh Mistry <rishabh90993@gmail.com>
2023-04-09 17:34:56 +0530

    Initial Commit Setup of the project

---
+++ src/test/java/com/example/urlShortner/Url/Shortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlShortner.Url.Shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
